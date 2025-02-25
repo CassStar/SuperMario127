416a73c11c128139a676e4d1382e79eef6f944fd
CassStar <163167577+CassStar@users.noreply.github.com>
2025-02-25 08:10:08 -0600

    Remove unnecessary check
    Since timers are paused when collecting a shine, this check isn't needed.

--- scenes/player/ui_new/timers/timer.gd
+++ scenes/player/ui_new/timers/timer.gd
@@ -109,10 +109,10 @@ func kill_player():
 	var player = get_node("/root").get_node("Player").get_node(get_node("/root").get_node("Player").character)
 	var player2 = get_node("/root").get_node("Player").get_node_or_null(get_node("/root").get_node("Player").character2)
 	if is_instance_valid(player):
-		if !player.dead and !player.shine_cutscene:
+		if !player.dead:
 			player.kill("timer")
 	if is_instance_valid(player2):
-		if !player2.dead and !player2.shine_cutscene:
+		if !player2.dead:
 			player2.kill("timer")
 
 func set_label(new_text: String):
